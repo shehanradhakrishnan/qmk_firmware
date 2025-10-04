856063b7f9380339688b038e750d1888cb88eb03
shehanradhakrishnan <98019627+shehanradhakrishnan@users.noreply.github.com>
2025-10-04 23:41:00 +1000

    New Layout
    Updated keymap with a new layout for 2025

--- keyboards/anavi/macropad8/keymaps/shehan/keymap.c
+++ keyboards/anavi/macropad8/keymaps/shehan/keymap.c
@@ -2,125 +2,156 @@
 
 enum layers {
   _MAIN,
+  _APPS,
   _MEDIA,
   _VLC,
-  _WARFRAME,
-  _FFXIV,
-  _FN
+  _LINUX,
+  _FN,
 };
 
+enum custom_keycodes {     
+    STEAM = SAFE_RANGE,     // SAFE_RANGE prevents it from using a clashing keycode
+    VLC,
+    FIREFOX,
+    VSCODE,
+    WARFRAME,
+    EVERCRISIS,
+};
+
+bool process_record_user(uint16_t keycode, keyrecord_t *record) {
+    switch (keycode) {
+        case STEAM:
+            if (record->event.pressed) {
+                SEND_STRING(SS_LGUI("r") SS_DELAY(500) "steam://open/library" SS_DELAY(500) SS_TAP(X_ENT));
+            } else {
+                // when keycode is released
+            }
+            break;
+        case VLC:
+            if (record->event.pressed) {
+                SEND_STRING(SS_LGUI("r") SS_DELAY(500) "vlc" SS_DELAY(500) SS_TAP(X_ENT));
+            } else {
+                // when keycode is released
+            }
+            break;
+        case FIREFOX:
+            if (record->event.pressed) {
+                SEND_STRING(SS_LGUI("r") SS_DELAY(500) "firefox" SS_DELAY(500) SS_TAP(X_ENT));
+            } else {
+                // when keycode is released
+            }
+            break;
+        case VSCODE:
+            if (record->event.pressed) {
+                SEND_STRING(SS_LGUI("r") SS_DELAY(500) "code" SS_DELAY(500) SS_TAP(X_ENT));
+            } else {
+                // when keycode is released
+            }
+            break;
+        case WARFRAME:
+            if (record->event.pressed) {
+                SEND_STRING(SS_LGUI("r") SS_DELAY(500) "steam://rungameid/230410" SS_DELAY(500) SS_TAP(X_ENT));
+            } else {
+                // when keycode is released
+            }
+            break;
+        case EVERCRISIS:
+            if (record->event.pressed) {
+                SEND_STRING(SS_LGUI("r") SS_DELAY(500) "steam://rungameid/2484110" SS_DELAY(500) SS_TAP(X_ENT));
+            } else {
+                // when keycode is released
+            }
+            break;
+    }
+    return true;
+}
+
 // #define _MAIN 0
-// #define _MEDIA 1
-// #define _VLC 2
-// #define _WARFRAME 3
-// #define _FFXIV 4
+// #define _APPS 1
+// #define _MEDIA 2
+// #define _VLC 3
+// #define _LINUX 4
 // #define _FN 5
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   /* Keymap _MAIN: (Base Layer) Default Layer
-   * ,---------------------------,
-   * |    TO(1)    | Ctl+Sft+ESC |
-   * |---------------------------|
-   * |    Ctl+C    |    Ctl+V    |
-   * |---------------------------|
-   * |    Win+D    |    Win+A    |
-   * |---------------------------|
-   * |  Sft+Win+S  | Win+Prt_Scr |
-   * `---------------------------`
+   * ,-------------------------------------------------------,
+   * | Ctl+Sft+ESC |    Calc.    |    My PC    |   TO(_FN)   |
+   * |-------------------------------------------------------|
+   * |  TO(_APPS)  | TO(_MEDIA)  |  TO(_VLC)   | TO(_LINUX)  |
+   * `-------------------------------------------------------`
    */
   [_MAIN] = LAYOUT_ortho_2x4(
-     RCS(KC_ESC), LCTL(KC_V), LGUI(KC_A), LGUI(KC_PSCR),
-     TO(_MEDIA),  LCTL(KC_C), LGUI(KC_D), SGUI(KC_S)
+     RCS(KC_ESC), KC_CALC,    KC_MYCM,  TO(_FN),
+     TO(_APPS),   TO(_MEDIA), TO(_VLC), TO(_FN)
   ),
 
-  /* Keymap _MEDIA: Groove Music Layer
-   * ,---------------------------,
-   * |    TO(2)    |    Media    |
-   * |---------------------------|
-   * |    Prev.    |    Next     |
-   * |---------------------------|
-   * |    Play     |    Mute     |
-   * |---------------------------|
-   * |   Vol. D    |   Vol. U    |
-   * `---------------------------`
+  /* Keymap _APPS: Commonly Used Apps
+   * ,-------------------------------------------------------,
+   * |   Firefox   | MediaPlayer |  VLC Player |   VS Code   |
+   * |-------------------------------------------------------|
+   * |  TO(_MAIN)  |    Steam    |  Warframe   | Ever Crisis |
+   * `-------------------------------------------------------`
+   */
+  [_APPS] = LAYOUT_ortho_2x4(
+     FIREFOX,   KC_MSEL, VLC,      VSCODE,
+     TO(_MAIN), STEAM,   WARFRAME, EVERCRISIS
+  ),
+
+  /* Keymap _MEDIA: MS Music Player
+   * ,-------------------------------------------------------,
+   * | MediaPlayer |    Prev.    |    Play     |    Next     |
+   * |-------------------------------------------------------|
+   * |  TO(_MAIN)  |    Mute     |   Shuffle   |   Repeat    |
+   * `-------------------------------------------------------`
    */
   [_MEDIA] = LAYOUT_ortho_2x4(
-     KC_MSEL,  KC_MNXT, KC_MUTE, KC_F9,
-     TO(_VLC), KC_MPRV, KC_MPLY, KC_F8
+     KC_MSEL,   KC_MPRV, KC_MPLY,    KC_MNXT,
+     TO(_MAIN), KC_MUTE, LCTL(KC_H), LCTL(KC_T)
   ),
 
-  /* Keymap _VLC: VLC Layer
-   * ,---------------------------,
-   * |    TO(2)    |    Calcu    |
-   * |---------------------------|
-   * |    Prev.    |    Next     |
-   * |---------------------------|
-   * |    Play     |    Mute     |
-   * |---------------------------|
-   * |   Vol. D    |   Vol. U    |
-   * `---------------------------`
+  /* Keymap _VLC: VLC Media Player
+   * ,-------------------------------------------------------,
+   * |  VLC Player |    Prev.    |    Play     |    Next     |
+   * |-------------------------------------------------------|
+   * |  TO(_MAIN)  |    Mute     |  Prv. Chap  |  Nxt. Chap  |
+   * `-------------------------------------------------------`
    */
-  // Later change CALC to macro that opens VLC player using Win+R
   [_VLC] = LAYOUT_ortho_2x4(
-     KC_CALC,       KC_MNXT, KC_M,    LCTL(KC_UP),
-     TO(_WARFRAME), KC_MPRV, KC_MPLY, LCTL(KC_DOWN)
+     VLC,       KC_MPRV, KC_MPLY,    KC_MNXT,
+     TO(_MAIN), KC_MUTE, LSFT(KC_P), LSFT(KC_N)
   ),
 
-  /* Keymap _WARFRAME: For Warframe
-   * ,---------------------------,
-   * |    TO(3)    |     F1      |
-   * |---------------------------|
-   * |     F2      |     F3      |
-   * |---------------------------|
-   * |     F4      |     F5      |
-   * |---------------------------|
-   * |     F7      |     F8      |
-   * `---------------------------`
+  /* Keymap _LINUX: For Fedora OS
+   * ,-------------------------------------------------------,
+   * |     F1      |     F2      |     F3      |     F4      |
+   * |-------------------------------------------------------|
+   * |  TO(_MAIN)  |     F5      |     F7      |     F8      |
+   * `-------------------------------------------------------`
    */
-  [_WARFRAME] = LAYOUT_ortho_2x4(
-     KC_F1,      KC_F3, KC_F5, KC_F8,
-     TO(_FFXIV), KC_F2, KC_F4, KC_F7
-  ),
-
-  /* Keymap _FFXIV: For Final Fantasy 14 (Hotbar 2: 1-6 + 12)
-   * ,---------------------------,
-   * |    TO(4)    |    Ctl+`    |
-   * |---------------------------|
-   * |    Ctl+1    |    Ctl+2    |
-   * |---------------------------|
-   * |    Ctl+3    |    Ctl+4    |
-   * |---------------------------|
-   * |    Ctl+5    |    Ctl+6    |
-   * `---------------------------`
-   */
-  [_FFXIV] = LAYOUT_ortho_2x4(
-     LCTL(KC_GRV), LCTL(KC_2), LCTL(KC_4), LCTL(KC_6),
-     TO(_FN)     , LCTL(KC_1), LCTL(KC_3), LCTL(KC_5)
-  ),
+  // [_LINUX] = LAYOUT_ortho_2x4(
+  //    KC_F1,     KC_F2, KC_F3, KC_F4,
+  //    TO(_MAIN), KC_F5, KC_F7, KC_F8
+  // ),
 
   /* Keymap _FN: Backlight & RGB Function Layer
-   * ,---------------------------,
-   * |    TO(0)    |   RGB Tog.  |
-   * |---------------------------|
-   * |   BL. Tog.  |  RGB Mode + |
-   * |---------------------------|
-   * |   BL. Inc.  |  RGB Mode - |
-   * |---------------------------|
-   * |   BL. Dec.  |  Chg Color  |
-   * `---------------------------`
+   * ,-------------------------------------------------------,
+   * |   RGB Tog.  |  RGB Mode + |  RGB Mode - |  Chg Color  |
+   * |-------------------------------------------------------|
+   * |  TO(_MAIN)  |   BL. Tog.  |   BL. Inc.  |   BL. Dec.  |
+   * `-------------------------------------------------------`
    */
   [_FN] = LAYOUT_ortho_2x4(
      RGB_TOG,   RGB_MOD, RGB_RMOD, RGB_HUI,
-     TO(_MAIN), BL_TOGG, BL_INC,  BL_DEC
+     TO(_MAIN), BL_TOGG, BL_UP,    BL_DOWN
   )
 };
 
 #ifdef OLED_ENABLE
-/* Don't Rotate Screen
+/* Don't Rotate Screen */
 oled_rotation_t oled_init_user(oled_rotation_t rotation) {
     return OLED_ROTATION_180;  // flips the display 180 degrees if offhand
 }
-*/
 
 bool oled_task_user(void) {
   // Host Keyboard Layer Status
@@ -131,17 +162,17 @@ bool oled_task_user(void) {
     case _MAIN:
       oled_write_ln_P(PSTR("Main"), false);
       break;
+    case _APPS:
+      oled_write_ln_P(PSTR("Programs"), false);
+      break;
     case _MEDIA:
       oled_write_ln_P(PSTR("Media"), false);
       break;
     case _VLC:
       oled_write_ln_P(PSTR("VLC"), false);
       break;
-    case _WARFRAME:
-      oled_write_ln_P(PSTR("Warframe"), false);
-      break;
-    case _FFXIV:
-      oled_write_ln_P(PSTR("FFXIV"), false);
+    case _LINUX:
+      oled_write_ln_P(PSTR("Fedora"), false);
       break;
     case _FN:
       oled_write_ln_P(PSTR("RGB"), false);
